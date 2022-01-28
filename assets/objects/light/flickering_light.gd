bcc4fb792729f8fe953d2afb0f0d208ef7f34bae
Johann NOVAK <johann.novak25@gmail.com>
2022-01-29 00:18:03 +0100

    Added flickering light script and curve

---
+++ assets/objects/light/flickering_light.gd
@@ -0,0 +1,34 @@
+extends SpotLight
+
+class_name FlickeringLight
+
+# Declare member variables here. Examples:
+export var flicker_enabled : bool = false
+export var flicker_threshold_random : float = 0
+var flicker_threshold : float = 0
+export var curve : Curve
+export var curve_time_scale : float = 1
+var flicker_duration = 2
+var flicker_elapsed_time_on = 0
+
+var light_energy_init = 1;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    light_energy_init = self.light_energy
+    flicker_threshold = rand_range(0.0, flicker_threshold_random)
+    
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void :
+    if not flicker_enabled:
+        return
+        
+    flicker_elapsed_time_on += delta;
+    if flicker_elapsed_time_on > flicker_threshold:
+        if flicker_elapsed_time_on > flicker_threshold + curve_time_scale:
+            flicker_elapsed_time_on = 0.0;
+            light_energy = light_energy_init
+            flicker_threshold = rand_range(0.0, flicker_threshold_random)
+        else:
+            var curve_pos = (flicker_elapsed_time_on - flicker_threshold) * curve_time_scale
+            light_energy = 1.0 if curve.interpolate(curve_pos) > 0.5 else 0.0
