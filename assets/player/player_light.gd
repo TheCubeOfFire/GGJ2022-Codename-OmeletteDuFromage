936167ae124d5c55077adf39041e0b139180d338
Julien Barbier <barbier.julien@orange.fr>
2022-01-29 22:06:42 +0100

    Added adaptative speed and scale to the core blob

--- assets/player/player_light.gd
+++ assets/player/player_light.gd
@@ -15,11 +15,15 @@ signal on_die
 
 
 var current_life := max_life
-
+var current_speed : float;
+var max_speed : float;
+var origin_scale : Vector3;
 
 func _ready() -> void:
-    pass
-
+    var coreNode = get_parent().get_child(0).get_child(0) as Spatial;
+    current_speed = coreNode.get("maxSpeed");
+    max_speed = current_speed;
+    origin_scale = coreNode.get_scale();
 
 func _process(delta: float) -> void:
     if current_life <= 0.0:
@@ -27,6 +31,9 @@ func _process(delta: float) -> void:
         
     _update_current_life(delta)
     _update_light_intensity()
+    _update_speed_core()
+    _update_core_scale()
+    
     
     if current_life <= 0.0:
         emit_signal("on_die")
@@ -43,3 +50,15 @@ func _update_current_life(delta: float) -> void:
 
 func _update_light_intensity() -> void:
     light.light_energy = light_intensity_curve.interpolate(current_life / max_life)
+    
+func _update_speed_core() -> void:
+    var coreNode = get_parent().get_child(0).get_child(0);
+    current_speed = max_speed * current_life / max_life;
+    coreNode.set("maxSpeed", current_speed);
+    
+func _update_core_scale() -> void:
+    var coreNode = get_parent().get_child(0).get_child(0) as Spatial;
+    coreNode.scale = origin_scale * current_life / max_life;
+    coreNode.scale.x = max(coreNode.scale.x, 0.1);
+    coreNode.scale.y = max(coreNode.scale.y, 0.1);
+    coreNode.scale.z = max(coreNode.scale.z, 0.1);
