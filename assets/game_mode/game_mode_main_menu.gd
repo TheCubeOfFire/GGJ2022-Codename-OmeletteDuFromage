371ac4ad13dc1501265d0df7332ae837e16cabad
Johann NOVAK <johann.novak25@gmail.com>
2022-01-29 14:00:15 +0100

    added random color for blob before first dash

--- assets/game_mode/game_mode_main_menu.gd
+++ assets/game_mode/game_mode_main_menu.gd
@@ -3,35 +3,59 @@ extends Node
 
 # Declare member variables here. Examples:
 var blobMeshInstance : MeshInstance
-var color_tmp : Array = [0.0, 0.0, 0.0]
-var current_color_index : int = 0
-var sleep_duration : float = 0.1
+
+var sleep_duration : float = 5
 var sleep_elapsed_time : float = 0.0
 var sleeping : bool = false
 
 var color_r_value : float = 0.0
 var color_g_value : float = 0.0
 var color_b_value : float = 0.0
-var color_variation_factor : int = 0.1
-var color_variation_threshold : int = 0.6
 
+var color_variation_factor : float = 0.1
+var color_variation_threshold : float = 0.6
 
+var color_chosen : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     blobMeshInstance = find_node("blobMeshInstance", true)
     assert(null != blobMeshInstance)
-    
-    current_color_index = floor(rand_range(0.0, 3.0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+    if color_chosen:
+        return
+    
+    if Input.is_action_just_pressed("dash"):
+        color_chosen = true
+        return
+        
+    sleep_elapsed_time += delta
+    if sleep_elapsed_time > sleep_duration:
+        sleeping = false
+        sleep_elapsed_time = 0.0
+    
+    if sleeping:
+        return    
     
     if not color_variation_factor + color_r_value + color_g_value + color_b_value > color_variation_threshold:
         color_r_value += color_variation_factor
     else :
         color_r_value = 0.0
-        
-    var material_blob = blobMeshInstance.get_surface_material(0) as ShaderMaterial
-    var new_color = Color(color_r_value, color_g_value, color_b_value)
-    material_blob.set_shader_param("emission_color", new_color)
+        if not color_variation_factor + color_g_value + color_b_value > color_variation_threshold:
+            color_g_value += color_variation_factor
+        else:
+            color_g_value = 0.0
+            if not color_variation_factor + color_b_value > color_variation_threshold:
+                color_b_value += color_variation_factor
+            else:
+                color_r_value = 0.0
+                color_g_value = 0.0
+                color_b_value = 0.0
+            
+    
+    var new_color := Vector3(color_r_value, color_g_value, color_b_value)
+    var material_blob := blobMeshInstance.get_surface_material(0) as ShaderMaterial
+    material_blob.set_shader_param("emissionColor", new_color)
+
