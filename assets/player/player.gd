6f9d18eb3a10587334e8d444afe5f0aa506db22d
Fabien Matusalem <matusalemf@aol.com>
2022-01-29 11:12:31 +0100

    added charge time to dash

--- assets/player/player.gd
+++ assets/player/player.gd
@@ -7,19 +7,26 @@ const DRAG_FACTOR: float = 10.0
 const MOUSE_SENSIVITY: float = 5.0
 
 var velocity := Vector3.ZERO
+var can_dash := true
 
 onready var camera_target := $CameraTarget as Position3D
 onready var player_light := $PlayerLight as PlayerLight
+onready var dash_timer := $DashTimer as Timer
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	var connect_error := dash_timer.connect("timeout", self, "_enable_dash")
+	if connect_error != OK:
+		push_error("Error connecting dash timer")
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
 
 	var drag := -DRAG_FACTOR * velocity
 
-	if Input.is_action_just_pressed("dash"):
+	if can_dash and Input.is_action_just_pressed("dash"):
+		can_dash = false
+		dash_timer.start()
 		velocity += DASH_ACCELERATION * direction * delta
 
 	velocity = move_and_slide(velocity, Vector3.UP)
@@ -33,3 +40,6 @@ func _rotate_camera(rx: float, ry: float, scale: float) -> void:
 	camera_target.rotate_x(deg2rad(rx * scale))
 	camera_target.rotation.x = clamp(camera_target.rotation.x, - PI / 2.0, PI / 2.0)
 	self.rotate_y(deg2rad(ry * scale))
+
+func _enable_dash() -> void:
+	can_dash = true
