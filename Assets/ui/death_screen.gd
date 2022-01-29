14042e174b325c36aae16cc7f189a0ae05f087eb
Loic Mathiot <loic.mathiot70@gmail.com>
2022-01-29 12:00:16 +0100

    Added death screen and game mode player values overrides

---
+++ Assets/ui/death_screen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+class_name DeathScreen
+
+
+signal on_retry_pressed
+
+
+func _ready():
+	assert($VBoxContainer/ButtonRetry.connect("pressed", self, "_on_retry_pressed") == 0)
+
+
+func _on_retry_pressed() -> void:
+	emit_signal("on_retry_pressed")
